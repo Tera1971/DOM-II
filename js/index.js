8a94368eb66aa4aa2010615e7bad0a4c71c881a8
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-04-10 13:16:47 -0400

    nav animation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,36 @@
 // Your code goes here
+const nav = document.querySelector(".nav-container");
+nav.addEventListener("mouseenter", function(){
+    
+    nav.style.transform = "scale(1.6)";
+    nav.style.transition = "transform 1s";
+  })
+  
+  nav.addEventListener("mouseleave", function(){
+    nav.style.transform = "scale(1.5)";
+  })
+  
 const body = document.querySelector('body');
 
-body.addEventListener('click', (event) => {
+body.addEventListener('mouseenter', (event) => {
   body.style.backgroundColor = "coral";
   body.style.color = "white";
 })
+body.addEventListener('mouseleave', (event) => {
+    body.style.backgroundColor = "white";
+    body.style.color = "coral";
+  })
+const button1 = document.querySelector('#btn1');
+button1.addEventListener('click', function(event){
+  event.target.style.color = 'orange';
+});lack
 
+const button2 = document.querySelector('#btn2');
+button2.addEventListener('mouseover', function(event){
+  event.target.style.background = 'black';
+});
+
+const button3 = document.querySelector('#btn3');
+button3.addEventListener('dblclick', function(event){
+  event.target.style.borderColor = 'red';
+});
