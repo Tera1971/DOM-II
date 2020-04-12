c79fa8be55cb3eb65db532d93cacf61a827ba45d
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-04-12 19:56:42 -0400

    added some event listeners

--- js/index.js
+++ js/index.js
@@ -1,36 +1,39 @@
 // Your code goes here
+
+
 const nav = document.querySelector(".nav-container");
-nav.addEventListener("mouseenter", function(){
-    
-    nav.style.transform = "scale(1.6)";
-    nav.style.transition = "transform 1s";
-  })
-  
-  nav.addEventListener("mouseleave", function(){
+    nav.addEventListener("mouseenter", function(event){
     nav.style.transform = "scale(1.5)";
+    nav.style.transition = "transform 1s";
+    event.stopPropagation();
+     
   })
-  
-const body = document.querySelector('body');
+  nav.addEventListener("mouseleave", function(event){
+    nav.style.transform = "scale(1.0)";
+  })
 
-body.addEventListener('mouseenter', (event) => {
-  body.style.backgroundColor = "coral";
-  body.style.color = "white";
+  const navStop = document.querySelector('nav');
+  navStop.addEventListener('click', function(event){
+  event.preventDefault();
+  alert(`LET'S GOOOO!`);
 })
-body.addEventListener('mouseleave', (event) => {
-    body.style.backgroundColor = "white";
-    body.style.color = "coral";
+
+
+// const btn = document.querySelectorAll('.btn')
+//   btn.addEventListener('click', function(event){
+//    btn.style.backgroundColor = 'red';
+//    btn.style.color = "black";
+//  });
+
+
+const footer = document.querySelector('footer');
+ footer.addEventListener('mouseenter', function(event){
+  footer.style.backgroundColor = "red";
+  footer.style.color = "black";
+})
+ footer.addEventListener('mouseleave', function(event){
+    footer.style.backgroundColor = "purple";
+    footer.style.color = "red";
   })
-const button1 = document.querySelector('#btn1');
-button1.addEventListener('click', function(event){
-  event.target.style.color = 'orange';
-});lack
 
-const button2 = document.querySelector('#btn2');
-button2.addEventListener('mouseover', function(event){
-  event.target.style.background = 'black';
-});
 
-const button3 = document.querySelector('#btn3');
-button3.addEventListener('dblclick', function(event){
-  event.target.style.borderColor = 'red';
-});
